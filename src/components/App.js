f8d21ad2b1d0088fc6f4ea320373059beda551bb
a <a@a.com>
2020-06-17 10:49:59 -0400

    made video selectable

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import VideoList from '../components/VideoList'
 class App extends React.Component {
  
   //intializes state array
-  state = {videos: [] }
+  state = {videos: [], selectedVideo: null }
 
   onTermSubmit = async (term) =>{
     const response = await youtube.get('./search', {
@@ -19,11 +19,15 @@ class App extends React.Component {
     this.setState({videos: response.data.items})
   }
 
+  onVideoSelect = (video) => {
+    console.log('From the App!', video)
+  }
+
   render(){
     return (
       <div className="ui container" style={{margin: '5%'}}>
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos} />
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     )
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,9 @@
+import './VideoItem.css'
 import React from "react";
 
 const VideoItem = (props) => {
-  console.log(props.video);
   return (
-    <div className="item">
+    <div onClick={()=> props.onVideoSelect(props.video)} className="video-item item">
       <img className="ui image" src={props.video.snippet.thumbnails.default.url}></img>
       <div className="content">
           <div className="header">{props.video.snippet.title}</div>
